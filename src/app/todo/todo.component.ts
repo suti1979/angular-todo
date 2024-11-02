ff2f72173894728330ab02fbcf1d964a9399e23d
AndrasZold <andras.zold@gammaorg.hu>
2024-11-02 17:30:00 +0100

    use signals

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,71 +1,15 @@
-import { Component, OnInit } from '@angular/core'
-import { Todo } from './todo.model'
+import { Component, inject } from '@angular/core'
 import { FormsModule } from '@angular/forms'
-import { catchError } from 'rxjs/operators'
-import { of } from 'rxjs'
 import { TodoService } from './todo.service'
 import { JsonPipe } from '@angular/common'
-// import { TodoItemsService as TodoService } from '../api/api/todoItems.service'
-// import { TodoItem } from '../api/Api'
 @Component({
   selector: 'app-todo',
   standalone: true,
   imports: [FormsModule, JsonPipe],
   templateUrl: './todo.component.html'
 })
-export class TodoComponent implements OnInit {
-  todos: Todo[] = []
-  newTodoTitle: string = ''
-  nextId: number = 1
-  errorMessage: string = ''
-  loading: boolean = false
+export class TodoComponent {
+  todoService = inject(TodoService)
 
-  constructor(private todoService: TodoService) {}
-
-  ngOnInit() {
-    this.loading = true
-    this.getTodos()
-    this.loading = false
-  }
-
-  getTodos() {
-    this.todoService
-      .fetchTodos()
-      .pipe(
-        catchError((error) => {
-          console.error('Error fetching todos:', error)
-          this.errorMessage = 'Failed to load todos. Please try again later.'
-          return of([])
-        })
-      )
-      .subscribe((todos) => {
-        this.todos = todos
-      })
-  }
-
-  addTodo() {
-    this.todoService
-      .addTodo({
-        name: this.newTodoTitle,
-        isComplete: false
-      })
-      .subscribe(() => {
-        this.getTodos()
-        this.newTodoTitle = ''
-      })
-  }
-
-  deleteTodo(id: string) {
-    this.todoService.deleteTodo(id).subscribe(() => {
-      this.getTodos()
-    })
-  }
-
-  toggleComplete(todo: Todo) {
-    const updatedTodo = { ...todo, isComplete: !todo.isComplete }
-
-    this.todoService.updateTodo(updatedTodo).subscribe(() => {
-      this.getTodos()
-    })
-  }
+  newTodoTitle = ''
 }
--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,27 +1,114 @@
-import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
+import { Injectable, signal } from '@angular/core'
 import { Todo } from './todo.model'
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  private apiUrl = 'http://localhost:5224/api/TodoItems'
+  private todos = signal<Todo[]>([])
 
-  constructor(private http: HttpClient) {}
+  getTodos = this.todos.asReadonly()
 
-  fetchTodos() {
-    return this.http.get<Todo[]>(this.apiUrl)
+  addTodo(todo: string) {
+    const newTodo = {
+      name: todo,
+      isComplete: false,
+      id: Date.now().toString()
+    }
+    this.todos.update((todos) => [...todos, newTodo])
   }
 
-  addTodo(todo: Omit<Todo, 'id'>) {
-    return this.http.post<Omit<Todo, 'id'>>(this.apiUrl, todo)
+  toggleComplete(todo: Todo) {
+    this.todos.update((todos) =>
+      todos.map((t) => (t.id === todo.id ? { ...t, isComplete: !t.isComplete } : t))
+    )
   }
 
   deleteTodo(id: string) {
-    return this.http.delete(`${this.apiUrl}/${id}`)
+    this.todos.update((todos) => todos.filter((todo) => todo.id !== id))
   }
 
   updateTodo(todo: Todo) {
-    return this.http.put(`${this.apiUrl}/${todo.id}`, todo)
+    this.todos.update((todos) => todos.map((t) => (t.id === todo.id ? todo : t)))
   }
 }
+
+// // counter.service.ts
+// import { Injectable, signal } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class CounterService {
+//   // Create a signal to hold the counter state
+//   private count = signal(0);
+
+//   // Expose the count as a readonly signal
+//   getCount = this.count.asReadonly();
+
+//   increment() {
+//     this.count.update(count => count + 1);
+//   }
+
+//   decrement() {
+//     this.count.update(count => count - 1);
+//   }
+
+//   reset() {
+//     this.count.set(0);
+//   }
+// }
+
+// // counter-display.component.ts
+// import { Component, inject } from '@angular/core';
+// import { CounterService } from './counter.service';
+
+// @Component({
+//   selector: 'app-counter-display',
+//   standalone: true,
+//   template: `
+//     <div>
+//       <h2>Counter Display</h2>
+//       <p>Current Count: {{ counterService.getCount() }}</p>
+//     </div>
+//   `
+// })
+// export class CounterDisplayComponent {
+//   counterService = inject(CounterService);
+// }
+
+// // counter-controls.component.ts
+// import { Component, inject } from '@angular/core';
+// import { CounterService } from './counter.service';
+
+// @Component({
+//   selector: 'app-counter-controls',
+//   standalone: true,
+//   template: `
+//     <div>
+//       <h2>Counter Controls</h2>
+//       <button (click)="counterService.increment()">Increment</button>
+//       <button (click)="counterService.decrement()">Decrement</button>
+//       <button (click)="counterService.reset()">Reset</button>
+//     </div>
+//   `
+// })
+// export class CounterControlsComponent {
+//   counterService = inject(CounterService);
+// }
+
+// // app.component.ts
+// import { Component } from '@angular/core';
+// import { CounterDisplayComponent } from './counter-display.component';
+// import { CounterControlsComponent } from './counter-controls.component';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [CounterDisplayComponent, CounterControlsComponent],
+//   template: `
+//     <h1>Counter App</h1>
+//     <app-counter-display></app-counter-display>
+//     <app-counter-controls></app-counter-controls>
+//   `
+// })
+// export class AppComponent {}
